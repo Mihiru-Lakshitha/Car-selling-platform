23fe4a40bcfd9c196ff97d3b6cd59b8d96e87080
Lakmini-Bandarigodage <143235230+Lakmini-Bandarigodage@users.noreply.github.com>
2023-11-11 13:46:13 +0530

    Updating script.js
    Adding validation!

--- product card/static/script.js
+++ product card/static/script.js
@@ -1,5 +1,11 @@
 function placeBid() {
     const bidAmount = document.getElementById('bidAmount').value;
-    // You can implement your bidding logic here
-    alert('Bid placed: $' + bidAmount);
+
+  
+    if (!isNaN(bidAmount) && bidAmount > 0) {
+       
+        alert('Bid placed: Rs.' + bidAmount);
+    } else {
+        alert('Please enter a valid bid amount.');
+    }
 }
