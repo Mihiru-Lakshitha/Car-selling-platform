ed31c3aca7f1f6b8939c1e2739cee0f1190204d9
Tharindu sandaruwan <147791352+Sandaruwanakt@users.noreply.github.com>
2023-11-11 09:39:47 +0530

    Product deatails card

---
+++ product card/static/script.js
@@ -0,0 +1,5 @@
+function placeBid() {
+    const bidAmount = document.getElementById('bidAmount').value;
+    // You can implement your bidding logic here
+    alert('Bid placed: $' + bidAmount);
+}
