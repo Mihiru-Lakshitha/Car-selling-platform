b4c55c447ab3883725ae9ca0f5500ecfbe996c0e
Tharindu sandaruwan <147791352+Sandaruwanakt@users.noreply.github.com>
2023-11-12 10:00:10 +0530

    Update script.js
    add bidding option

--- Car listing page/static/script.js
+++ Car listing page/static/script.js
@@ -1,8 +1,32 @@
+// JavaScript (script.js)
 function toggleDescription(carId) {
     var description = document.getElementById(carId).getElementsByClassName('car-description')[0];
     if (description.style.display === 'none' || description.style.display === '') {
         description.style.display = 'block';
     } else {
         description.style.display = 'none';
+    }
+}
+
+function showBidForm(carId) {
+    var bidForm = document.getElementById(carId).getElementsByClassName('bid-form')[0];
+    bidForm.style.display = 'block';
+}
+
+function makeBid(carId) {
+    var bidAmountInput = document.getElementById(carId).getElementsByClassName('bid-amount')[0];
+    var bidAmount = bidAmountInput.value;
+
+    if (bidAmount.trim() === "") {
+        alert("Please enter a valid bid amount.");
+    } else {
+        alert('Bid placed for ' + carId + ' with amount Rs.' + bidAmount);
+
+        // Optionally, you can reset the input field after the bid is submitted
+        bidAmountInput.value = "";
+
+        // Hide the bid form after submission
+        var bidForm = document.getElementById(carId).getElementsByClassName('bid-form')[0];
+        bidForm.style.display = 'none';
     }
 }
