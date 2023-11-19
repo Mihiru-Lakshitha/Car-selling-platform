857184667c308ee54453a78889f36cd819575096
Deshan Weerakkody <147709113+Deshan989@users.noreply.github.com>
2023-11-19 11:15:55 +0530

    Update models.py

--- auctions/models.py
+++ auctions/models.py
@@ -12,7 +12,6 @@ class AuctionListing(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     starting_bid = models.DecimalField(max_digits=12, decimal_places=2, blank=True)
     category = models.CharField(max_length=100, blank=True)
     image_url = models.URLField(default='https://user-images.githubusercontent.com/52632898/161646398-6d49eca9-267f-4eab-a5a7-6ba6069d21df.png')
@@ -20,6 +19,18 @@ class AuctionListing(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
     winner = models.CharField(max_length=100, blank=True, null=True)
+    edition = models.CharField(max_length=100, blank=True)
+    year = models.IntegerField(blank=True, null=True)
+    condition = models.CharField(max_length=100, blank=True)
+    no_of_owners = models.IntegerField(blank=True, null=True)
+    transmission = models.CharField(max_length=100, blank=True)
+    engine_capacity = models.DecimalField(max_digits=5, decimal_places=1, blank=True, null=True)
+    fuel_type = models.CharField(max_length=100, blank=True)
+    weight = models.DecimalField(max_digits=5, decimal_places=1, blank=True, null=True)
+    mileage = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    district = models.CharField(max_length=100, blank=True) 
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
 
     def __str__(self):
         return f'{self.title}: by {self.user.username}'
