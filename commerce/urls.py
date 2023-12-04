53c7eb4f05345fecfa3662af375fa91f80b3e200
Mihiru Lakshitha Silva <66376006+Mihiru-Lakshitha@users.noreply.github.com>
2023-12-04 21:46:35 +0530

    Update urls.py

--- commerce/urls.py
+++ commerce/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 from home.views import HomeView  # Import the home view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,3 +12,6 @@ urlpatterns = [
 ]
 
 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
