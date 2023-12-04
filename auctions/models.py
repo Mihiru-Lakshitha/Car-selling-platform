46e35ec5fd350fbd6221ea8982033b29fc610975
Mihiru Lakshitha Silva <66376006+Mihiru-Lakshitha@users.noreply.github.com>
2023-12-04 21:51:03 +0530

    Update models.py

--- auctions/models.py
+++ auctions/models.py
@@ -5,7 +5,12 @@ from django.db import models
 class User(AbstractUser):
     watchlist_counter = models.IntegerField(default=0, blank=True)
     watchlist = models.ManyToManyField('AuctionListing', related_name='watchlist', blank=True)
-    pass
+    phone = models.CharField(max_length=15, blank=True)
+    zip = models.CharField(max_length=10, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', default='Guest.png', blank=True)
+
+    def __str__(self):
+        return self.username
 
 
 class AuctionListing(models.Model):
@@ -14,7 +19,7 @@ class AuctionListing(models.Model):
     description = models.TextField()
     starting_bid = models.DecimalField(max_digits=12, decimal_places=2, blank=True)
     category = models.CharField(max_length=100, blank=True)
-    image_url = models.URLField(default='https://user-images.githubusercontent.com/52632898/161646398-6d49eca9-267f-4eab-a5a7-6ba6069d21df.png')
+    image_url = models.URLField(default='https://images-platform.99static.com/pzfVOCfWSdCWxplUquFVXeUNeZs=/500x500/top/smart/99designs-contests-attachments/28/28767/attachment_28767406')
     bid_counter = models.IntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
