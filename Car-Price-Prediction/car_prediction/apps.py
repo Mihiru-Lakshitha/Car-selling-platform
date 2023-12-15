108e229b2d0daab8a6851766c125c5cd6368e51c
Mihiru Lakshitha Silva <66376006+Mihiru-Lakshitha@users.noreply.github.com>
2023-12-15 20:27:39 +0530

    Delete Car-Price-Prediction/car_prediction directory

--- Car-Price-Prediction/car_prediction/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CarPredictionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'car_prediction'
--- Car-Price-Prediction/car_prediction/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import CarInput
-
-class CarInputForm(forms.ModelForm):
-    class Meta:
-        model = CarInput
-        fields = ['mileage', 'num_owners', 'year', 'starting_bid']
-
